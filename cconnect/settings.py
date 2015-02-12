d952ac65861334263fc4edc1c2136f88df9ebaf6
Vivek R <vividvilla@gmail.com>
2015-02-12 11:36:50 +0530

    Added db settings

--- cconnect/settings.py
+++ cconnect/settings.py
@@ -58,8 +58,10 @@ WSGI_APPLICATION = 'cconnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cconnect',
+        'USER': 'vivek',
+        'PASSWORD': 'lastsupper'
     }
 }
 
